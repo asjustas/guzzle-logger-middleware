0573ddd9b227f60a66a1097141d40927437d93db
Justas Vitaitis <justas@u3.lt>
2018-07-20 01:13:35 +0300

    Update example

--- example/example.php
+++ example/example.php
@@ -2,97 +2,17 @@
 
 include 'vendor/autoload.php';
 
-use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\HandlerStack;
+use AJ\Guzzle\Middleware\Logger;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use GuzzleHttp\Promise;
-
-class LoggerMiddleware {
-    /** @var callable */
-    private $callback;
-
-    /** @var bool */
-    private $enabled;
-
-    /** @var array */
-    private $context;
-
-    /**
-     * LoggerMiddleware constructor.
-     * @param callable $callback
-     */
-    public function __construct(callable $callback)
-    {
-        $this->callback = $callback;
-    }
-
-    public function __invoke(callable $handler)
-    {
-        return function ($request, array $options) use ($handler) {
-            $this->setOptions($options);
-
-            return $handler($request, $options)->then(
-                $this->onSuccess($request),
-                $this->onFailure($request)
-            );
-        };
-    }
-
-    protected function onSuccess(RequestInterface $request)
-    {
-        return function ($response) use ($request) {
-            if ($this->enabled) {
-                $this->log($request, $response);
-            }
-
-            return $response;
-        };
-    }
-
-    protected function onFailure(RequestInterface $request)
-    {
-        return function ($reason) use ($request) {
-            if ($this->enabled) {
-                $response = null;
-
-                if ($reason instanceof RequestException) {
-                    $response = $reason->getResponse();
-                }
-
-                $this->log($request, $response);
-            }
-
-            return Promise\rejection_for($reason);
-        };
-    }
-
-    protected function log(RequestInterface $request, ResponseInterface $response = null)
-    {
-        call_user_func(
-            $this->callback,
-            $request,
-            $response,
-            $this->context
-        );
-    }
-
-    private function setOptions(array $options)
-    {
-        if (!isset($options['logger'])) {
-            return;
-        }
-
-        $this->enabled = $options['logger']['enabled'] ?? false;
-        $this->context = $options['logger']['context'] ?? [];
-    }
-}
+use GuzzleHttp\Client;
 
 try {
     $stack = HandlerStack::create();
     $stack
         ->push(
-            new LoggerMiddleware(
+            new Logger(
                 function (RequestInterface $request, ?ResponseInterface $response, array $context) {
                     print_r(
                         [
@@ -107,7 +27,7 @@ try {
             )
         );
 
-    $client = new \GuzzleHttp\Client();
+    $client = new Client();
 
     $client
         ->request(
